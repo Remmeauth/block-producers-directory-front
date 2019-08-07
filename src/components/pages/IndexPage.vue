f66f2561115838b60a724f86f5f6f8d4a8ec2bcb
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-08-07 14:43:31 +0300

    Create index page modules

--- src/components/pages/IndexPage.vue
+++ src/components/pages/IndexPage.vue
@@ -1,10 +1,47 @@
 <template>
-    <div>{{ 'index page' }}</div>
+  <div>
+    {{ 'index page' }}
+    <BlockProducers />
+      {{ error.message }}
+      {{ error.statusCode }}
+
+      {{ blockProducers }}
+    </div>
 </template>
 
 <script>
+import store from '../../store/index'
+import BlockProducers from '../common/BlockProducers'
+import {
+  INDEX_PAGE_ADD_BLOCK_PRODUCERS_MUTATION,
+  INDEX_PAGE_ADD_ERROR_MUTATION
+} from '../../store/modules/indexPage'
+
 export default {
   name: 'IndexPage',
+  components: {
+    BlockProducers
+  },
+  data() {
+    return {
+      error: {
+        message: null,
+        statusCode: null,
+      },
+      blockProducers: null
+    }
+  },
+  mounted() {
+    store.subscribe((mutation, state) => {
+      if (mutation.type === INDEX_PAGE_ADD_ERROR_MUTATION) {
+        this.error = state.indexPage.error
+      }
+
+      if (mutation.type === INDEX_PAGE_ADD_BLOCK_PRODUCERS_MUTATION) {
+        this.blockProducers = state.indexPage.blockProducers
+      }
+    });
+  },
 }
 </script>
 
