cc155ced69d95879baa72f5c1bd4234d98f488ff
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-08-11 22:42:34 +0300

    Create module for block producer edition with actions and mutations

--- src/store/modules/blockProducer.js
+++ src/store/modules/blockProducer.js
@@ -1,43 +1,113 @@
 import axios from 'axios'
 import HttpStatus from 'http-status-codes'
 
-export const ADD_ERROR_MUTATION = 'addError'
-export const ADD_BLOCK_PRODUCER_MUTATION = 'addBlockProducer'
-export const BLOCK_PRODUCER_ADD_ERROR_MUTATION = 'blockProducer/' + ADD_ERROR_MUTATION
-export const BLOCK_PRODUCER_ADD_BLOCK_PRODUCER_MUTATION = 'blockProducer/' + ADD_BLOCK_PRODUCER_MUTATION
-export const BLOCK_PRODUCER_GET_ACTION = 'blockProducer/get'
+export const blockProducerStorageMutations = {
+  subscribe: {
+    addError: 'blockProducer/addError',
+    addFieldsErrors: 'blockProducer/addFieldsErrors',
+    getBlockProducer: 'blockProducer/getBlockProducer',
+    updateDetails: 'blockProducer/updateDetails',
+    updateDescription: 'blockProducer/updateDescription',
+    updateReferenceLinks: 'blockProducer/updateReferenceLinks',
+  },
+  commit: {
+    addError: 'addError',
+    addFieldsErrors: 'addFieldsErrors',
+    getBlockProducer: 'getBlockProducer',
+    updateDetails: 'updateDetails',
+    updateDescription: 'updateDescription',
+    updateReferenceLinks: 'updateReferenceLinks',
+  },
+}
 
+export const blockProducerStorageActions = {
+  getBlockProducer: 'blockProducer/get',
+  updateDetails: 'blockProducer/updateDetails',
+  updateDescription: 'blockProducer/updateDescription',
+  updateReferenceLinks: 'blockProducer/updateReferenceLinks',
+}
 
 export const blockProducer = {
   namespaced: true,
   state: {
-    blockProducer: {
-      name: null,
-      location: null,
-      shortDescription: null,
-      fullDescription: null,
-      logoUrl: null,
-      facebookUrl: null,
-      githubUrl: null,
-      linkedInUrl: null,
-      redditUrl: null,
-      mediumUrl: null,
-      steemitUrl: null,
-      telegramUrl: null,
-      slackUrl: null,
-      twitterUrl: null,
-      websiteUrl: null,
-      wikipediaUrl: null,
-      user: null,
-      id: null,
+    error: {
+      message: null,
+      statusCode: null,
     },
+    fieldsErrors: {
+      errors: null,
+      statusCode: null,
+    },
+    isUpdated: false,
+    name: null,
+    location: null,
+    shortDescription: null,
+    fullDescription: null,
+    logoUrl: null,
+    facebookUrl: null,
+    githubUrl: null,
+    linkedInUrl: null,
+    redditUrl: null,
+    mediumUrl: null,
+    steemitUrl: null,
+    telegramUrl: null,
+    slackUrl: null,
+    twitterUrl: null,
+    websiteUrl: null,
+    wikipediaUrl: null,
+    user: null,
+    id: null,
   },
   mutations: {
     addError (state, error) {
       state.error = error
     },
-    addBlockProducer (state, blockProducer) {
-      state.blockProducer = blockProducer
+    addFieldsErrors (state, errors) {
+      state.fieldsErrors = errors
+    },
+    getBlockProducer (state, { 
+      name,
+      location,
+      shortDescription,
+      fullDescription,
+      logoUrl,
+      facebookUrl,
+      githubUrl,
+      linkedInUrl,
+      redditUrl,
+      mediumUrl,
+      steemitUrl,
+      telegramUrl,
+      slackUrl,
+      twitterUrl,
+      websiteUrl,
+      wikipediaUrl,
+    }) {
+      state.name = name
+      state.location = location
+      state.shortDescription = shortDescription
+      state.fullDescription = fullDescription
+      state.logoUrl = logoUrl
+      state.facebookUrl = facebookUrl
+      state.githubUrl = githubUrl
+      state.linkedInUrl = linkedInUrl
+      state.redditUrl = redditUrl
+      state.mediumUrl = mediumUrl
+      state.steemitUrl = steemitUrl
+      state.telegramUrl = telegramUrl
+      state.slackUrl = slackUrl
+      state.twitterUrl = twitterUrl
+      state.websiteUrl = websiteUrl
+      state.wikipediaUrl = wikipediaUrl
+    },
+    updateDetails (state, isUpdated) {
+      state.isUpdated = isUpdated
+    },
+    updateDescription (state, isUpdated) {
+      state.isUpdated = isUpdated
+    },
+    updateReferenceLinks (state, isUpdated) {
+      state.isUpdated = isUpdated
     },
   },
   actions: {
@@ -45,7 +115,7 @@ export const blockProducer = {
       axios
         .get(`https://bps-directory-back-staging.herokuapp.com/block-producers/${identifier}/`)
         .then(response => {
-          commit(ADD_BLOCK_PRODUCER_MUTATION, {
+          commit(blockProducerStorageMutations.commit.getBlockProducer, {
             name: response.data.result.name,
             location: response.data.result.location,
             shortDescription: response.data.result.short_description,
@@ -68,20 +138,140 @@ export const blockProducer = {
         })
         .catch(error => {
           if (error.response.status === HttpStatus.INTERNAL_SERVER_ERROR) {
-            commit(ADD_ERROR_MUTATION, {
+            commit(blockProducerStorageMutations.commit.addError, {
               message: error.response.data.error,
               statusCode: error.response.status
             })
           }
 
           if (error.response.status === HttpStatus.NOT_FOUND) {
-            commit(ADD_ERROR_MUTATION, {
+            commit(blockProducerStorageMutations.commit.addError, {
               message: error.response.data.error,
               statusCode: error.response.status
             })
           }
         })
-    }
+    },
+    updateDetails({ commit }, { 
+      identifier, 
+      name, 
+      location,
+    }) {
+      axios
+        .post(`https://bps-directory-back-staging.herokuapp.com/block-producers/${identifier}/`, {
+          name: name,
+          location: location,
+        }, {
+            headers: {
+                'Authorization': `JWT ${window.localStorage.token.slice(1, -1)}`,
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => {
+          commit(blockProducerStorageMutations.commit.updateDetails, true)
+        })
+        .catch(error => {
+          if (error.response.status === HttpStatus.INTERNAL_SERVER_ERROR) {
+            commit(blockProducerStorageMutations.commit.addError, {
+              message: error.response.data.error,
+              statusCode: error.response.status
+            })
+          }
+
+          if (error.response.status === HttpStatus.BAD_REQUEST) {
+            commit(blockProducerStorageMutations.commit.addFieldsErrors, {
+              errors: error.response.data.errors,
+              statusCode: error.response.status
+            })
+          }
+        })
+    },
+    updateDescription({ commit }, { 
+      identifier, 
+      shortDescription, 
+      fullDescription, 
+    }) {
+      axios
+        .post(`https://bps-directory-back-staging.herokuapp.com/block-producers/${identifier}/`, {
+          short_description: shortDescription,
+          full_description: fullDescription,
+        }, {
+            headers: {
+                'Authorization': `JWT ${window.localStorage.token.slice(1, -1)}`,
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => {
+          commit(blockProducerStorageMutations.commit.updateDescription, true)
+        })
+        .catch(error => {
+          if (error.response.status === HttpStatus.INTERNAL_SERVER_ERROR) {
+            commit(blockProducerStorageMutations.commit.addError, {
+              message: error.response.data.error,
+              statusCode: error.response.status
+            })
+          }
+
+          if (error.response.status === HttpStatus.BAD_REQUEST) {
+            commit(blockProducerStorageMutations.commit.addFieldsErrors, {
+              errors: error.response.data.errors,
+              statusCode: error.response.status
+            })
+          }
+        })
+    },
+    updateReferenceLinks({ commit }, {
+        identifier, 
+        facebookUrl,
+        githubUrl,
+        linkedInUrl,
+        redditUrl,
+        mediumUrl,
+        steemitUrl,
+        telegramUrl,
+        slackUrl,
+        twitterUrl,
+        websiteUrl,
+        wikipediaUrl,
+      }) {
+      axios
+        .post(`https://bps-directory-back-staging.herokuapp.com/block-producers/${identifier}/`, {
+          facebook_url: facebookUrl,
+          github_url: githubUrl,
+          linkedin_url: linkedInUrl,
+          reddit_url: redditUrl,
+          medium_url: mediumUrl,
+          steemit_url: steemitUrl,
+          telegram_url: telegramUrl,
+          slack_url: slackUrl,
+          twitter_url: twitterUrl,
+          website_url: websiteUrl,
+          wikipedia_url: wikipediaUrl,
+        }, {
+            headers: {
+                'Authorization': `JWT ${window.localStorage.token.slice(1, -1)}`,
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => {
+          commit(blockProducerStorageMutations.commit.updateReferenceLinks, true)
+        })
+        .catch(error => {
+          if (error.response.status === HttpStatus.INTERNAL_SERVER_ERROR) {
+            commit(blockProducerStorageMutations.commit.addError, {
+              message: error.response.data.error,
+              statusCode: error.response.status
+            })
+          }
+
+          if (error.response.status === HttpStatus.BAD_REQUEST) {
+            commit(blockProducerStorageMutations.commit.addFieldsErrors, {
+              errors: error.response.data.errors,
+              statusCode: error.response.status
+            })
+          }
+        })
+    },
   }
 }
 
