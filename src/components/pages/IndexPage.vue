65b908978f47aa4c980722db2d4c28a7ee5d0626
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-08-12 15:38:23 +0300

    Add block producers search to index page

--- src/components/pages/IndexPage.vue
+++ src/components/pages/IndexPage.vue
@@ -4,7 +4,22 @@
       {{ error.message }}
       {{ error.statusCode }}
 
+        <form>
+        <v-container grid-list-xl fluid>
+          <v-layout wrap>
+            <v-flex xs12 sm6 md4>
+              <v-text-field v-model="searchPhrase" label="phrase" solo required ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </form>
+
+      {{ blockProducersSearch }}
+
+      <br><br>
+
       {{ blockProducers }}
+
     </div>
 </template>
 
@@ -15,6 +30,8 @@ import {
   INDEX_PAGE_ADD_BLOCK_PRODUCERS_MUTATION,
   INDEX_PAGE_ADD_ERROR_MUTATION
 } from '../../store/modules/indexPage'
+
+import { blockProducerStorageActions, blockProducerStorageMutations } from '../../store/modules/blockProducer'
 
 export default {
   name: 'IndexPage',
@@ -27,7 +44,16 @@ export default {
         message: null,
         statusCode: null,
       },
-      blockProducers: null
+      blockProducers: null,
+      blockProducersSearch: null,
+      searchPhrase: null,
+    }
+  },
+  watch: {
+    searchPhrase: function (val) {
+      store.dispatch(blockProducerStorageActions.searchBlockProducers, {
+        phrase: this.searchPhrase = val,
+      })
     }
   },
   mounted() {
@@ -38,6 +64,18 @@ export default {
 
       if (mutation.type === INDEX_PAGE_ADD_BLOCK_PRODUCERS_MUTATION) {
         this.blockProducers = state.indexPage.blockProducers
+      }
+
+      if (mutation.type === blockProducerStorageMutations.subscribe.addError) {
+        this.error = state.blockProducer.error
+      }
+
+      if (mutation.type === blockProducerStorageMutations.subscribe.addFieldsErrors) {
+        this.fieldsErrors = state.blockProducer.fieldsErrors
+      }
+
+      if (mutation.type === blockProducerStorageMutations.subscribe.searchBlockProducers) {
+        this.blockProducersSearch = state.blockProducer.blockProducersSearch
       }
     });
   },
