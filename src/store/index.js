2f3a0b090cb03b4b515fc09ed6de3df487a89c27
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-08-20 19:04:44 +0300

    Refactor routes, unsubscribe, back-end ulrs

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,11 @@ import Vue from 'vue'
 import authentication from './modules/authentication'
 import avatar from './modules/avatar'
 import blockProducer from './modules/blockProducer'
-import blockProducerCreation from './modules/blockProducerCreation'
 import comment from './modules/comment'
-import indexPage from './modules/indexPage'
 import like from './modules/like'
 import profile from './modules/profile'
 import settings from './modules/settings'
 import user from './modules/user'
-import userPage from './modules/userPage'
 
 Vue.use(Vuex)
 
@@ -20,14 +17,11 @@ const store = new Vuex.Store({
     authentication: authentication,
     avatar: avatar,
     blockProducer: blockProducer,
-    blockProducerCreation: blockProducerCreation,
     comment: comment,
-    indexPage: indexPage,
     like: like,
     profile: profile,
     settings: settings,
     user: user,
-    userPage: userPage
   }
 })
 
