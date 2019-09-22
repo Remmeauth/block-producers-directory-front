1ea045b9823b15001b1f3b054ecc4f9b5da40d84
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-09-22 19:15:08 +0300

    Create module for change user password with actions and mutations

--- src/store/modules/password.js
+++ src/store/modules/password.js
@@ -7,18 +7,21 @@ export const passwordStorageMutations = {
     addFieldsErrors: 'password/addFieldsErrors',
     sendNewPasswordToEmail: 'password/sendNewPasswordToEmail',
     receivePasswordRecoveryRequest: 'password/receivePasswordRecoveryRequest',
+    changePassword: 'password/changePassword',
   },
   commit: {
     addError: 'addError',
     addFieldsErrors: 'addFieldsErrors',
     sendNewPasswordToEmail: 'sendNewPasswordToEmail',
     receivePasswordRecoveryRequest: 'receivePasswordRecoveryRequest',
+    changePassword: 'changePassword',
   },
 }
 
 export const passwordStorageActions = {
   sendNewPasswordToEmail: 'password/sendNewPasswordToEmail',
   getPasswordRecoveryRequest: 'password/getPasswordRecoveryRequest',
+  changePassword: 'password/changePassword',
 } 
 
 
@@ -36,6 +39,7 @@ export const password = {
     events: {
       isSent: null,
       isReceived: null,
+      isChanged: null,
     },
   },
   getters: {
@@ -48,8 +52,39 @@ export const password = {
     addFieldsErrors: (state, errors) => state.fieldsErrors = errors,
     sendNewPasswordToEmail: (state) => state.events.isSent = Math.random(),
     receivePasswordRecoveryRequest: (state) => state.events.isReceived = Math.random(),
+    changePassword: (state) => state.events.isChanged = Math.random(),
   },
   actions: {
+    changePassword({ commit }, { username, jwtToken, oldPassword, newPassword }) {
+      axios
+        .post(process.env.VUE_APP_BACK_END_URL + `/users/${username}/password/`, {
+          old_password: oldPassword,
+          new_password: newPassword,
+        }, {
+          headers: {
+            'Authorization': `JWT ${jwtToken}`,
+            'Content-Type': 'application/json',
+          }
+        })
+        .then(response => {
+          commit(passwordStorageMutations.commit.changePassword)
+        })
+        .catch(error => {
+          if (error.response.status === HttpStatus.INTERNAL_SERVER_ERROR) {
+            commit(passwordStorageMutations.commit.addError, {
+              message: error.response.data.error,
+              statusCode: error.response.status
+            })
+          }
+
+          if (error.response.status === HttpStatus.BAD_REQUEST) {
+            commit(passwordStorageMutations.commit.addFieldsErrors, {
+              errors: error.response.data.error,
+              statusCode: error.response.status
+            })
+          }
+        })
+    },
     getPasswordRecoveryRequest({ commit }, { email }) {
       axios
         .post(process.env.VUE_APP_BACK_END_URL + '/users/password/recovery/', {
